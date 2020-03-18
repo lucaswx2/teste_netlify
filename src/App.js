aca6f724c9ee1797eed8b6d9566c5efc09730edd
Lucas Medeiros <freitasmlucas@gmail.com>
2020-03-18 15:38:00 -0300

    Primeiro commit testando netlify

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [empresa, setEmpresa] = useState("Segredo");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Olá, esta aplicação foi subida no netlify - {empresa}</h1>
+      <button
+        type="button"
+        onClick={() => setEmpresa("Avariti - Tecnologia e Negócios")}
+      >
+        Clique aqui para ver a empresa
+      </button>
+    </>
   );
 }
 
